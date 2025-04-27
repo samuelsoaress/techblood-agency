c70e64763ca0a79bead4fa53126d0d284254fd80
Samuel Soares da Silva <samuelsoares177778@gmail.com>
2025-04-27 19:50:28 -0300

    Create scripts-deploy.sh

---
+++ scripts-deploy.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+docker load -1 techblood-agency.tar
+
+mv docker-compose-prod.yaml docker-compose.yaml
+
+container_ids=$(docker ps -q)
+
+if [ -z "$container_ids" ]; then
+  echo "Nao ha containers em execucao"
+else
+  for container_id in $container_ids; do
+    echo "Parando container: $container_id"
+    docker stop $container_id
+  done
+  echo "Todos os containers em execucao foram parados. "
+fi 
+
+docker-compose up -d
