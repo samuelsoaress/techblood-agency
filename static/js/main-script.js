8b2f21837f3028705563d3afa0e579009fae6af1
samuelsoaress <samuelsoares177778@gmail.com>
2025-05-05 21:04:24 -0300

    feat: correcao menu mobile

--- static/js/main-script.js
+++ static/js/main-script.js
@@ -1,15 +1,31 @@
-const toggleBtn = document.getElementById('menu-toggle');
-const mobileMenu = document.getElementById('mobile-menu');
-const overlay = document.getElementById('menu-overlay');
+document.addEventListener('DOMContentLoaded', () => {
+  const toggleBtn = document.getElementById('menu-toggle');
+  const mobileMenu = document.getElementById('ekit-megamenu-menu-principal');
+  const overlay = document.getElementById('menu-overlay');
+  const closeButton = document.getElementById('menu-close');
 
-toggleBtn.addEventListener('click', () => {
-  toggleBtn.classList.toggle('active');
-  mobileMenu.classList.toggle('active');
-  overlay.classList.toggle('active');
-});
+  if (toggleBtn && mobileMenu && overlay && closeButton) {
+    
+    closeButton.addEventListener('click', () => {
+      toggleBtn.classList.remove('active');
+      mobileMenu.classList.remove('active');
+      overlay.classList.remove('active');
+    });
 
-overlay.addEventListener('click', () => {
-  toggleBtn.classList.remove('active');
-  mobileMenu.classList.remove('active');
-  overlay.classList.remove('active');
+    // Adiciona o evento de clique ao botão de menu
+    // para abrir o menu
+    toggleBtn.addEventListener('click', () => {
+      toggleBtn.classList.toggle('active');
+      mobileMenu.classList.toggle('active');
+      overlay.classList.toggle('active');
+    });
+
+    overlay.addEventListener('click', () => {
+      toggleBtn.classList.remove('active');
+      mobileMenu.classList.remove('active');
+      overlay.classList.remove('active');
+    });
+  } else {
+    console.warn('Algum dos elementos do menu não foi encontrado no DOM.');
+  }
 });
